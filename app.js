a70d0f3b10e9c3f5c7561ff491bff14d017e9b6c
msaimsohail <116447889+msaimsohail@users.noreply.github.com>
2022-11-03 04:55:24 -0700

    Hello World

---
+++ app.js
@@ -0,0 +1,23 @@
+// alert("Personal Details")
+// var username = +prompt("Enter Your username")
+// var Fathername = +prompt("Enter your Fathername")
+// var Class = +prompt("Enter your class")
+
+// var Maths = +prompt("Enter yout maths marks")
+// var Physics = +prompt("Enter yout physics marks")
+// var Chemistry = +prompt("Enter yout chemistry marks")
+// var English = +prompt("Enter your english marks")
+// var Computer = +prompt("Enter yout computer marks")
+// var totalmarks = 500;
+// var total = (Maths+Physics+Chemistry+English+Computer)
+// var percentage = total / totalmarks * 100;
+
+// if (percentage > 45 && percentage < 50 ) {
+//     console.log("Your grade is C")
+// }else if (percentage >=50 && percentage < 60) {
+//     console.log("Your grade is B")
+// }else if (percentage >= 60 && percentage < 70) {
+//     console.log("Your grade is A")
+// }else if (percentage >= 70 && percentage < 80) {
+//     console.log("Your grade is Aone")
+// }
